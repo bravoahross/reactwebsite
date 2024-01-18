2ce7922c94d22c5d767d4572b2f071f2f7dcc3db
ncross3392 <ncross4@iastate.edu>
2024-01-17 23:33:16 -0600

    Working header, pages are blank

---
+++ src/pages/Experience.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import { MenuList } from "../helpers/MenuList";
+// import MenuItem from "../components/MenuItem";
+import "../styles/Experience.css";
+
+function Experience() {
+  return (
+    <div className="experience">
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+        <p> fnjdafknaskdjf </p>
+      </div>
+    </div>
+  );
+}
+
+export default Experience;
